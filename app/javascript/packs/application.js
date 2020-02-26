ec1077f55d9faf163219bec3aaa559947a3c112b
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2020-02-26 23:45:05 +0900

    再現に不要な修正を削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,1 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
-import '../src'
-// Support component names relative to this directory:
-/* var componentRequireContext = require.context("components", true);
- * var ReactRailsUJS = require("react_ujs");
- * ReactRailsUJS.useContext(componentRequireContext); */
+import 'react-bootstrap-table'
--- app/javascript/src/index.js
+++
@@ -1,8 +0,0 @@
-import 'core-js/stable'
-import 'regenerator-runtime/runtime'
-import 'bootstrap'
-import './style'
-
-const ctx = require.context('./components', true);
-import ReactRailsUJS from 'react_ujs';
-ReactRailsUJS.useContext(ctx);
